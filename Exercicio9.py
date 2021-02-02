cd49a8d37542d1d821488c3e952db77c9e9db0b0
Priscila Banqueri <priscilabanqueri@Priscilas-MacBook-Pro.local>
2021-02-02 20:07:46 -0300

    Finalizei  o exercicio 9

---
+++ Exercicio9.py
@@ -0,0 +1,6 @@
+#crie uma variavel com uma saudacao e outra com seu nome
+#imprima a saudacao completa por meio de concatenacao exemplo: Ola Sr. Marcos.
+
+saudacao = "Ola "
+nome = "Priscila"
+print(saudacao + nome)
