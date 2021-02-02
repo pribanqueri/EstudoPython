915050f9f3cfce4b4a95b0327e8166daf2752c42
Priscila Banqueri <priscilabanqueri@Priscilas-MacBook-Pro.local>
2021-02-02 19:40:18 -0300

    Finalizei o exercicio 7

---
+++ Exercicio7.py
@@ -0,0 +1,12 @@
+#escreva uma expressao que verifica se o aluno atingiu a media 7
+#crie 3 variaveis com notas diferentes
+#de um print no resultado
+
+nota1 = 5
+nota2 = 7
+nota3 = 10
+media = 7
+
+print(nota1 >= media)
+print(nota2 >= media)
+print(nota3 >= media)
