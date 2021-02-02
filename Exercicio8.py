397dfb481e1d9228ac8aa71c8a2be408e9e3c97e
Priscila Banqueri <priscilabanqueri@Priscilas-MacBook-Pro.local>
2021-02-02 19:50:50 -0300

    Finalizei o exercicio 8

---
+++ Exercicio8.py
@@ -0,0 +1,9 @@
+#crie uma variavel salario com o valor da sua escolha
+#crie uma variavel aumento com o valor da sua escolha
+#atribua a variavel novo salario o valor do novo salario com aumento
+#exiba o resultado na tela
+
+salario = 11000
+aumento = 1000.50
+novosalario = salario + aumento
+print(novosalario)
