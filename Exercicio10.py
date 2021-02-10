78944d29fd8b7694f02792f87f849d1b98165f0c
Priscila Banqueri <priscilabanqueri@Priscilas-MacBook-Pro.local>
2021-02-09 21:07:06 -0300

    Exercicio 10 e 11 finalizado

---
+++ Exercicio10.py
@@ -0,0 +1,11 @@
+#criar uma variavel com o nome profissao e colocar o nome da profissao
+
+profissao = "reporter"
+print(profissao)
+
+print("reporter")
+
+frase = "mete uma frase louca aí"
+print(len(frase))
+
+print(len("qual o tamanho dessa frase"))
---
+++ Exercicio11.py
@@ -0,0 +1,15 @@
+nome = "PRiscila"
+print(len(nome))
+print(nome[0])
+print(nome[7])
+
+
+sobrenome = "Banqueri"
+
+print(nome + sobrenome)
+
+print("Ola " + nome + " " + sobrenome)
+
+espaco = " "
+print(nome + espaco + sobrenome)
+
